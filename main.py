a2713dbb7219289aac63ad1f50c514a170ffd702
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-12-04 01:27:57 +0300

    feat: доработана точка входа

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 
+from external_storage.kafka_storage import KafkaStorage
+from external_storage.postgres_broker import PostgresStorage
+
 if __name__ == "__main__":
-    palace_to_send = os.environ.get("PLACE_TO_SEND", "postress")  # or kafka
-    agr_time = os.environ.get("AGR_PARAM")  # time from env
+    storage_type = os.environ.get("PLACE_TO_SEND", "postgres")  # or kafka
+    agr_time = os.environ.get("AGR_TIME")  # time from env
+    url = os.environ.get("URL")
+    if storage_type == "postgres":
+        login = input("Enter login: ")
+        password = input("Enter password: ")
+        storage = PostgresStorage(url=url, login=login, password=password)
+    else:
+        storage = KafkaStorage(url=url)
 
 
